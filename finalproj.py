1fc5f9d109725f06dd806aa0f453ccc0be3746ad
Roshan Shaikh <roshanrizwan1414@gmail.com>
2026-06-02 19:53:55 +0530

    Update finalproj.py

--- finalproj.py
+++ finalproj.py
@@ -5,4 +5,16 @@ sn.bgcolor("blue")
 sn.setup(width=600, height=600)
 sn.tracer(0)
 
+#head ig
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("green")
+head.penup()
+head.goto(0, 0)
+head.direction = "stop"
+
+while True:
+    sn.update()
+
 sn.mainloop()
